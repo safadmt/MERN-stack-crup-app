d700f5712e1f6b38b49becea3c5cb0af45ca9497
Safad mt <116543401+safadmt@users.noreply.github.com>
2023-06-05 17:55:13 +0530

    Update Home.js

--- client/src/component/home/Home.js
+++ client/src/component/home/Home.js
@@ -1,7 +1,7 @@
 import react , {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom'
 import axios from 'axios';
-import '../../App.css'
+
 const Home = () => {
     const [user, setUser] = useState([]);
 
@@ -44,7 +44,7 @@ const Home = () => {
                         return <tr key={index}>
                         <td>{index+1}</td>
                         <td>{obj.name}</td>
-                        <td><img src={require(`../../../../server/public/images/${obj.image}`)} id='tdimage'></img></td>
+                        <td><img src={require(`http://localhost:4000/images/${obj.image}`)} id='tdimage'></img></td>
                         <td>{obj.address}</td>
                         <td><Link to={`/edit-user/${obj._id}`} className='btn btn-outline-primary'>Edit</Link></td>
                         <td><button className="btn btn-outline-danger" onClick={(e)=>handleDelete(obj._id,e)}>Delete</button></td>
